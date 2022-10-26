bfd3a2cf4cdae9dde144495e67377e2f787acac2
unknown <muh.it.eng.19841984@gmail.com>
2022-10-26 09:56:30 +0300

    add other endpoint

--- main.py
+++ main.py
@@ -45,5 +45,45 @@ async def create(request: Request):
     body = await request.json()
     delivery = Delivery(budget=body['data']['budget'], notes=body['data']['notes']).save()
     event = Event(delivery_id=delivery.pk, type=body['type'], data=json.dumps(body['data'])).save()
-   
-    return event
+    state = consumers.CONSUMERS[event.type]({}, event)
+    # store in redis cache
+    redis.set(f'delivery:{delivery.pk}', json.dumps(state))
+    return state
+
+@app.get('/deliveries/{pk}/status')
+async def get_state(pk: str):
+    state = redis.get(f'delivery:{pk}')
+
+    if state is not None:
+        # json.loads to convert string to object json
+        return json.loads(state)
+
+    state = build_state(pk)
+     # json.dumps to convert object json to string
+    redis.set(f'delivery:{pk}', json.dumps(state))
+    return state
+
+
+@app.post('/event')
+async def dispatch(request: Request):
+    body = await request.json()
+    delivery_id = body['delivery_id']
+    state = await get_state(delivery_id)
+    event = Event(delivery_id=delivery_id, type=body['type'], data=json.dumps(body['data'])).save()
+    new_state = consumers.CONSUMERS[event.type](state, event)
+    redis.set(f'delivery:{delivery_id}', json.dumps(new_state))
+    return new_state
+
+
+
+def build_state(pk: str):
+    pks = Event.all_pks()
+    all_events = [Event.get(pk) for pk in pks]
+    events = [event for event in all_events if event.delivery_id == pk]
+    state = {}
+
+    for event in events:
+        state = consumers.CONSUMERS[event.type](state, event)
+
+    return state
+
